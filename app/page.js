156f647a831d6ff1bc84fcf03bacfae608bfd3d0
shahriyor123445 <boyzoqovshahriyor@gmail.com>
2024-08-03 01:10:31 +0500

    a

--- app/page.js
+++ app/page.js
@@ -3,8 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <Link href={"/complexs-dashboard"}>home</Link>
+    <main className="min-h-screen text-center pt-40">
+      <Link
+        className="bg-blue-500 p-6 border rounded-md text-white"
+        href={"/complexs-dashboard"}
+      >
+        Go to Complexs-Dashboard
+      </Link>
     </main>
   );
 }
